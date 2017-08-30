05efbfb425a7fc7eee5f0dffa99c4c551962fc8d
Aidan Dowling <adowling.student14@stmarys.ie>
2017-08-30 14:19:31 +0100

    Base

---
+++ bin/fileopen.py
@@ -0,0 +1,7 @@
+import random as rnd
+"""USED FOR OPENING AND INTERPRETING ANSWERS AS VARIABLES"""
+class readFile(object):
+    def __init__(self):
+        print("Working")
+    def file(self, name):
+        fileObj=open("trainingdata.txt","r")
