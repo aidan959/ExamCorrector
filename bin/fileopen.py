89ec6c315e83cb044b842e9071351d3ab2ea2afe
Aidan Dowling <adowling.student14@stmarys.ie>
2017-09-02 13:46:55 +0100

    File Reader/Parser added

--- bin/fileopen.py
+++ bin/fileopen.py
@@ -1,7 +1,32 @@
-import random as rnd
-"""USED FOR OPENING AND INTERPRETING ANSWERS AS VARIABLES"""
-class readFile(object):
-    def __init__(self):
-        print("Working")
-    def file(self, name):
-        fileObj=open("trainingdata.txt","r")
+#pylint: disable=invalid-name
+#USED FOR OPENING AND INTERPRETING ANSWERS AS VARIABLES
+
+
+#Reads the file and creates a list of classifiers(questions) and their meanings
+def rFile(name):
+    oneShot = False
+    fileObj = open(name, "r")
+    #Temporary Variables
+    currentclassifier = ""
+    linesnclassifiers = {}
+    classifierlist = []
+    filelines = []
+    with fileObj as f:
+        for line in f:
+            line = line.replace("\n","")
+            #Checks if is classifier 
+            if "[" and "]" in line or oneShot is False:
+                oneShot = True
+                line = line.replace("[", "")
+                line = line.replace("]", "")
+                classifierlist.append(line)
+                currentclassifier = line
+                
+                filelines=[]
+            else:
+                if line != "":
+                    filelines.append(line)
+                    linesnclassifiers[currentclassifier] = filelines
+                
+       
+        return linesnclassifiers
