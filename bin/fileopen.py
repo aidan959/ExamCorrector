0d6569deffbf2b7c14c1cd16190e4ab6e386a51b
Aidan Dowling <adowling.student14@stmarys.ie>
2017-11-06 18:41:08 +0000

    Added more conditions and checked for issues

--- bin/fileopen.py
+++ bin/fileopen.py
@@ -1,4 +1,6 @@
 """ USED FOR OPENING AND INTERPRETING ANSWERS AS VARIABLES"""
+import MySQLdb
+
 
 # Reads the file and creates a list of classifiers(questions) and their meanings
 def read_sample_answers(name):
@@ -32,8 +34,9 @@ def read_sample_answers(name):
                         lines_and_classifiers[currentclassifier] = filelines
 
         return lines_and_classifiers
-def read_answer(name):
-    """Opens answers from the students."""
+#Am now using mySQL
+"""def read_answer(name):
+    Opens answers from the students.
     # Temporary Variables
     one_shot = False
     file_obj = open(name, "r")
@@ -58,4 +61,29 @@ def read_answer(name):
                     filelines.append(line)
                     lines_and_question[current_question] = filelines
 
-        return lines_and_question
+        return lines_and_question"""
+def get_students():
+    databaseconnection = MySQLdb.connect(host="localhost",  # your host
+                                         user="examiner",       # username
+                                         passwd="aidan",     # password
+                                         db="examcorrector")   # name of the database
+    cursor = databaseconnection.cursor()
+    examnumbers=[]
+    cursor.execute("SELECT exam_number FROM students")
+    for row in cursor.fetchall():
+       print row
+def read_answers(exam_number):
+    """Function to read from database"""
+    get_students()
+    databaseconnection = MySQLdb.connect(host="localhost",  # your host
+                                         user="examiner",       # username
+                                         passwd="aidan",     # password
+                                         db="examcorrector")   # name of the database
+    cursor = databaseconnection.cursor()
+
+    cursor.execute("SELECT answers FROM students WHERE exam_number = " + str(exam_number))
+    for row in cursor.fetchall():
+        answers = row[0].split("//")
+    return answers
+
+print read_answers(123456)
--- html/index.php
+++ html/index.php
@@ -8,27 +8,52 @@ $questions = fopen("questions.txt", "r") or die("Unable to open file!");
 $ids= array();
 $weberrors=array();
 $answers=array();
+$issue=false;
+//Reads the text file line by line
 while (!feof($questions)) {
+    
+    //gets the line
     $line = fgets($questions);
-    if(strpos($line,'[') !== false){
+    
+    //checks if the line has a [ and ]
+    if(strpos($line,'[') !== false and strpos($line,']') !== false){
+        
+        //removes those brackets
         $line = str_replace("[", "", $line, $i);
         $line = str_replace("]", "", $line, $i);
+        
+        //pushs the id to the array
         array_push($ids, $line);
-        //CHECKS IF IT LISTS THE IDS RIGHT
+        
+        //UNCOMMENT TO CHECK IF IT LISTS THE IDS RIGHT
         //echo $line, "<br>";
     }
     
- }
+}
+//Checks if submit has been pressed
 if(isset($_POST['submit'])) {
+    //Opens the database connection
     $conn = new PDO($dsn, $username, $password);
+
+    //Gets the examnumber and centre from the site
     $enumber = $_POST['examnumber'];
     $ecnumber = $_POST['examcentre'];
+    //gets each id from the array, and uses the id to call from the page
+    
     foreach($ids as &$id)
     {
-        array_push($answers, $_POST['q'+$id]);
+        //checks if ID is empty and 
+        if (empty($_POST['q'+$id])){
+            echo "empty!";
+        }
+        else{
+            array_push($answers, $_POST['q'+$id]);
+        }
+        //joins the arrays with a //
         $dbanswers= implode("//", $answers);
     }
-    if(empty($enumber) or empty($ecnumber))
+    //checks if the important values are empty
+    if(empty($enumber) or empty($ecnumber) or $issue == true)
     {
         if(empty($enumber))
         {
